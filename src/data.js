d3cc5d2ae72c9626c76081e334cfd426a3c7dbb8
Malcolm Stone <malcolm.stone91@gmail.com>
2015-03-10 21:59:54 +0000

    Initial styling for website

--- src/data.js
+++ src/data.js
@@ -8,6 +8,16 @@ var soundboardItems = [
 		id: "yaman-1",
 		text: "Yaman! 1",
 		src: "Yaman!-1.mp3"
+	},
+	{
+		id: "oh-my-god-1",
+		text: "Oh My God 1",
+		src: "Oh-My-God-1.mp3"
+	},
+	{
+		id: "yello-1",
+		text: "Yello! 1",
+		src: "Yello!-1.mp3"
 	}
 ];
 
