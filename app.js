def927583e08869db053d8cdbdfd15a4050f28fc
Malcolm Stone <malcolm.stone@geeks.ltd.uk>
2015-03-17 18:19:46 +0000

    Fixed port issue

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var data 	= require("./src/data.js");
 var app 	= express();
 
 // config
-
+app.set("port", (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, "public")));
 
 // views
@@ -22,7 +22,7 @@ app.get("/bollocks", function (req, res) {
 	});
 });
 
-var server = app.listen(3001, function () {
+var server = app.listen(app.get("port"), function () {
 	var host = server.address().address;
 	var port = server.address().port;
 
