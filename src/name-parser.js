4056b87c207337a1cccbf66da4e92ab1976e9fbc
Malcolm Stone <malcolm.stone91@gmail.com>
2015-04-12 00:03:42 +0100

    Unit tests passing!

--- src/name-parser.js
+++ src/name-parser.js
@@ -1,45 +1,73 @@
-var replaceDictionary = [
-	{
-		key: "youve",
-		value: "you've"
-	},
-	{
-		key: "dont",
-		value: "don't"
-	},
-	{
-		key: "cant",
-		value: "can't"
-	},
-	{
-		key: "Cant",
-		value: "Can't"
-	},
-	{
-		key: "Dont",
-		value: "Don't"
-	},
-	{
-		key: /-/g,
-		value: " "
-	},
-	{
-		key: ".mp3",
-		value: ""
+function replace(target, pattern, replaceFunc) {
+	var match = target.match(pattern);
+	if (match) {
+		return replaceFunc(match);
+	} else {
+		return target;
 	}
-];
+}
 
-[1,2,3,4,5,6,7,8,9].forEach(function(i) {
-	replaceDictionary.push({
-		key: i.toString(),
-	 	value: "#" + i
-	});
-});
-
-module.exports = function parse(name) {
-	replaceDictionary.forEach(function(kv) {
-		name = name.replace(kv.key, kv.value);
-	});
-
+function capitalise(name) {
+	if (name[0])
+		return name[0].toUpperCase() + name.substring(1);
 	return name;
 }
+
+function fixApostrophes (name) {
+	return name
+		.replace("ouve", "ou've")
+		.replace("ont", "on't")
+		.replace("ant", "an't")
+		.replace("im", "i'm")
+		.replace("ive", "i've");
+}
+
+function hashTagNumbers (word) {
+	if (parseInt(word)) {
+		return "#" + word;
+	} else {
+		return word;
+	}
+}
+
+module.exports = function parse(name) {
+	var words = name
+		.replace(".mp3", "")
+		.split("-");
+
+	return words
+		.map(fixApostrophes)
+		.map(hashTagNumbers)
+		.map(capitalise)
+		.join(" ");
+}
+
+//var replaceDictionary = [
+//	{
+//		key: "youve",
+//		value: "you've"
+//	},
+//	{
+//		key: "dont",
+//		value: "don't"
+//	},
+//	{
+//		key: "cant",
+//		value: "can't"
+//	},
+//	{
+//		key: "Cant",
+//		value: "Can't"
+//	},
+//	{
+//		key: "Dont",
+//		value: "Don't"
+//	}
+//];
+//
+//[1,2,3,4,5,6,7,8,9].forEach(function(i) {
+//	replaceDictionary.push({
+//		key: i.toString(),
+//	 	value: "#" + i
+//	});
+//});
--- test/name-parser-fixture.js
+++ test/name-parser-fixture.js
@@ -9,31 +9,19 @@ describe('name-parser', function() {
 		});
 
 		it('should remove .mp3', function() {
-			assert.equal(parse("test.mp3"), "test");
+			assert.equal(parse("test.mp3"), "Test");
 		});
 
-		it('should fix youve', function() {
-			assert.equal(parse("you've"), "you've");
-		});
-
-		it('should fix dont', function() {
-			assert.equal(parse("dont"), "don't");
-		});
-
-		it('should fix cant', function() {
-			assert.equal(parse("cant"), "can't");
+		it('should fix apostrophes', function() {
+			assert.equal(parse("youve"), "You've");
+			assert.equal(parse("dont"), "Don't");
+			assert.equal(parse("cant"), "Can't");
+			assert.equal(parse("im"), "I'm");
+			assert.equal(parse("ive"), "I've");
 		});
 
 		it('should change 1 to #1', function() {
-			assert.equal(parse("test-1.mp3"), "test #1");
-		});
-
-		it('should handle capitalisations', function() {
-			assert.equal(parse("Dont"), "Don't")
-		});
-
-		it('should handle capitalisations', function() {
-			assert.equal(parse("Cant"), "Can't")
+			assert.equal(parse("test-1.mp3"), "Test #1");
 		});
 	});
 });
